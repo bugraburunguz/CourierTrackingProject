1c711949433d48d35b3744246764c41723c94cc3
bugraburunguz <bugra.burunguz@demirorenteknoloji.com>
2024-06-10 13:59:41 +0300

    Reviewed code

--- evaluation-service/src/main/java/com/couriertracking/evaluationservice/factory/HaversineFactory.java
+++ evaluation-service/src/main/java/com/couriertracking/evaluationservice/factory/HaversineFactory.java
@@ -2,10 +2,7 @@ package com.couriertracking.evaluationservice.factory;
 
 import com.couriertracking.evaluationservice.model.Haversine;
 import com.couriertracking.evaluationservice.model.impl.HaversineImpl;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
 
-import java.util.logging.Level;
 public class HaversineFactory {
     private HaversineFactory() {
         throw new UnsupportedOperationException("Utility class");
